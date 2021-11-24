8532a44d146ec2c6f00ec0dc70451aabb574749b
Rajan Walia <rajan.walia@scalenut.com>
2021-11-24 15:16:17 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const config = require("config");
+// const {connection} = require("./db");
+
+const PORT = config.get("PORT") || 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/",require("./routes"));
+app.get("/",(req,res)=>{
+    return res.send("Hello Everyone :)");
+})
+
+app.listen(PORT,async (err)=>{
+    if(err)
+        console.log("Error in starting the server");
+    
+    // await connection.connect((err)=>{
+    //     if(err){
+    //         console.log("Error in connection : ",err);
+    //         return;
+    //     }
+    //     console.log("Mysql connected");
+    // })
+    console.log(`server is started on http://localhost:${PORT} `);
+})
